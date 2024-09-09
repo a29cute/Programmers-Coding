c6f9e41a10f76a764b5986849c43de666ad66d02
a29cute <allsun0829@gmail.com>
2024-09-09 22:39:05 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+        for (int i : num_list) {
+            if (i == n) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
